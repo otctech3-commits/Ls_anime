920aa12adf747a879aed190cfc704a3dab7b5763
_SRI_ <otctech3@gmail.com>
2026-06-14 16:17:27 +0530

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   TG_BOT_TOKEN: '8332375866:AAFbQooQBVTTx-T4uBE_07rAUy1c79sy8JA',
-  TG_CHANNEL: '-1004391819492',
+  TG_CHANNEL: '@mkbots_anime', // USE @username NOT -100...
   ANIME_API: 'https://api.jikan.moe/v4',
   AUTO_PLAY: true
 };
