0ba60f45c59ee2e1656d78de04055572b10dc6e5
_SRI_ <otctech3@gmail.com>
2026-06-14 15:22:12 +0530

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,10 +1,10 @@
 // ===== CONFIGURATION =====
 const CONFIG = {
   // Telegram Bot Token - Get from @BotFather
-  TG_BOT_TOKEN: 'YOUR_BOT_TOKEN_HERE',
+  TG_BOT_TOKEN: '8332375866:AAFbQooQBVTTx-T4uBE_07rAUy1c79sy8JA',
 
   // Telegram Channel Username or ID
-  TG_CHANNEL: '@YOUR_CHANNEL', // or '-1001234567890'
+  TG_CHANNEL: '-1004391819492', // or '-1001234567890'
 
   // API to fetch anime details - FREE APIs
   ANIME_API: 'https://api.jikan.moe/v4', // Jikan API - MyAnimeList
